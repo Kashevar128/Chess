5de26c6e00465640b68e7700f61b5723fbc4924b
Kashevar128 <steve128@yandex.ru>
2021-03-02 22:49:22 +0300

    update 001

--- src/ChessFigure.java
+++ src/ChessFigure.java
@@ -1,2 +1,39 @@
-public class ChessFigure {
+public abstract class ChessFigure {
+
+    protected int x, y;
+    private final String color;
+
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract boolean isKnight();
+
+    public ChessFigure (int x, int y, String color) {
+        this.x = x;
+        this.y = y;
+        if (!color.equals("black") && !color.equals("white")) {
+            throw new RuntimeException("color should be black " +
+                    "or white");
+        }
+        this.color = color;
+    }
+
+    protected boolean isInBoard() {
+        return x >= 0 && y >= 0 && x < 8 && y < 8;
+    }
+    public abstract boolean can (int x,int y);
 }
---
+++ src/Knight.java
@@ -0,0 +1,26 @@
+public class Knight extends ChessFigure{
+
+    @Override
+    public boolean can(int x, int y) {
+        if (isInBoard()) {
+            int dx = x - getX();
+            int dy = y - getY();
+            return dx * dx + dy * dy == 5;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean isKnight() {
+        return true;
+    }
+
+    public Knight(int x, int y, String color) {
+        super(x, y, color);
+    }
+
+    @Override
+    public String toString() {
+        return "K";
+    }
+}
